74eb9d0db4078df6e81e0ce1fa26447d19ac537a
Roman Akera <rakera@livesoft.pro>
2023-03-12 14:47:30 +0300

    017 login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,16 +8,27 @@ import {
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponceInterface } from '@app/user/types/userResponce.interface';
+import { LoginUserDto } from '@app/user/dto/login.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post('users')
   @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponceInterface> {
     const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponce(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponceInterface> {
+    const user = await this.userService.login(loginUserDto);
     return this.userService.buildUserResponce(user);
   }
 }
