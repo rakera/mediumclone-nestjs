1269e1bff2cc4d4cd9a341e828a320636006b834
Roman Akera <rakera@livesoft.pro>
2023-03-13 23:42:18 +0300

    024 articles create pt.2

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,5 +1,6 @@
 import { ArticleService } from '@app/article/article.service';
 import { CreateArticleDto } from '@app/article/dto/createArticleDto';
+import { ArticleResponseInterface } from '@app/article/types/atricleResponse';
 import { User } from '@app/user/decorators/user.decorator';
 import { AuthGuard } from '@app/user/guards/auth.guards';
 import { UserEntity } from '@app/user/user.entity';
@@ -13,7 +14,11 @@ export class ArticleController {
   async create(
     @User() currentUser: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto,
-  ): Promise<any> {
-    return this.articleService.createArticle(currentUser, createArticleDto);
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,9 +1,11 @@
 import { ArticleEntity } from '@app/article/article.entity';
 import { CreateArticleDto } from '@app/article/dto/createArticleDto';
+import { ArticleResponseInterface } from '@app/article/types/atricleResponse';
 import { UserEntity } from '@app/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticleService {
@@ -23,10 +25,22 @@ export class ArticleService {
       article.tagList = [];
     }
 
-    article.slug = 'crutch';
+    article.slug = this.getSlug(createArticleDto.title);
 
     article.author = currentUser;
 
     return await this.articleRepository.save(article);
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
   }
 }
